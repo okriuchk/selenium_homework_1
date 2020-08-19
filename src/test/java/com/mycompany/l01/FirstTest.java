40531636c3bcc7e3c88983261fa0dcf1f81b4a58
Oksana.Kriuchkova <Oksana.Kriuchkova@t-systems.com>
2020-08-19 22:40:15 +0300

    delete import capabilities

--- src/test/java/com/mycompany/l01/FirstTest.java
+++ src/test/java/com/mycompany/l01/FirstTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.opera.OperaDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
